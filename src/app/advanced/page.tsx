6bfa4f6488d2f8a3afa64ed2a22b330c5c2ad762
takeshi sugimoto <doubtman.daru.231@gmail.com>
2026-04-09 13:28:35 +0900

    fix advanced page client component

--- src/app/advanced/page.tsx
+++ src/app/advanced/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import AdvancedInputForm from '@/components/forms/AdvancedInputForm';
 import PremiumModal from '@/components/PremiumModal';
